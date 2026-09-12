f05775caf4986abc47e66229d8a770b36a2b3b74
psurfschool-per <psurfschool@gmail.com>
2026-09-11 20:27:07 -0500

    fix: cache-busting js/css (?v=) + no-cache para js/css (Cloudflare servia main.js viejo)

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use(express.static(path.join(__dirname), {
   maxAge: '1d',
   etag: true,
   setHeaders: (res, filePath) => {
-    if (filePath.endsWith('.html')) {
+    if (filePath.endsWith('.html') || filePath.endsWith('.js') || filePath.endsWith('.css')) {
       res.setHeader('Cache-Control', 'no-cache');
     }
   }
